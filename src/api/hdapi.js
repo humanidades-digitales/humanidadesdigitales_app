38bea7e4fcecbb63de24dec182df9de7aa167926
gianpietro1 <gianpietro1@gmail.com>
2022-08-20 12:47:41 -0500

    xhange url api

--- src/api/hdapi.js
+++ src/api/hdapi.js
@@ -5,11 +5,11 @@ console.log(process.env.NODE_ENV);
 export const hdapi = axios.create({
   baseURL:
     process.env.NODE_ENV === 'production'
-      ? 'http://humanidadesdigitales.pe:3000'
+      ? 'http://humanidadesdigitales.pe/api'
       : 'http://localhost:3000',
 });
 
 export const downloadUrl =
   process.env.NODE_ENV === 'production'
-    ? 'http://humanidadesdigitales.pe:3000'
+    ? 'http://humanidadesdigitales.pe/api'
     : 'http://localhost:3000';
